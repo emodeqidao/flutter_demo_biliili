e979e2aa88598a29268eaa06546fb56be192028e
xixi <xixi_wen@foxmial.com>
2025-11-03 09:42:13 +0800

    create android dic

--- android/app/src/main/kotlin/com/xixi/demo/flutter_demo_bilibili/MainActivity.kt
+++ android/app/src/main/kotlin/com/xixi/demo/flutter_demo_bilibili/MainActivity.kt
@@ -2,5 +2,4 @@ package com.xixi.demo.flutter_demo_bilibili
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
---
+++ lib/state/state_demo_main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final DoubleController _controller = DoubleController(0.5);
+  final sc = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MainPage'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("hello", style: TextStyle(fontSize: 30),),
+          SubItem(
+            controller: _controller,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(onPressed: () {
+                _controller.setValue(1.0);
+              }, child: Text("100%")),
+              ElevatedButton(onPressed: () {
+                _controller.setValue(0.0);
+              }, child: Text("0%"))
+            ],
+          )
+
+        ],
+      ),
+    );
+  }
+}
+
+class DoubleController extends ChangeNotifier{
+  double _value;
+  DoubleController(this._value);
+
+  double get value => _value;
+
+  void setValue(double value){
+    _value = value;
+    notifyListeners();
+  }
+}
+
+class SubItem extends StatelessWidget {
+  final DoubleController controller;
+  const SubItem({super.key, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+    children: [
+      Text("12", style: TextStyle(fontSize: controller.value * 100 + 12),),
+      FlutterLogo(size:  controller.value * 100 + 50,),
+      ListenableBuilder(
+        listenable: controller,
+        builder: (BuildContext context, Widget? child) {
+        return Slider(value: controller.value, onChanged: (v) {
+          controller.setValue(v);
+          print(v);
+        });
+      },
+
+      ),
+    ],
+      );
+  }
+}
+
