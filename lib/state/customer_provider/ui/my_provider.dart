a4be77b62ebbefb53e3d1e2a3e119f9210794a2e
xixi <xixi_wen@foxmial.com>
2025-11-06 16:39:12 +0800

    add state T

--- lib/state/customer_provider/ui/my_provider.dart
+++ lib/state/customer_provider/ui/my_provider.dart
@@ -7,35 +7,10 @@ class MyProvider<T extends Listenable> extends StatefulWidget {
   const MyProvider({super.key, required this.child, required this.create});
 
   @override
-  State<MyProvider> createState() => _MyProviderState();
+  State<MyProvider> createState() => _MyProviderState<T>();
 
-  // static T? of<T> (BuildContext context) {
-  //   return context.dependOnInheritedWidgetOfExactType<MyInheritedWidget<T>>()?.model;
-  // }
-
-  static T? of<T>(BuildContext context) {
-    final inherited = context.dependOnInheritedWidgetOfExactType<MyInheritedWidget<T>>();
-    if (inherited != null) {
-      return inherited.model;
-    }
-
-    // 如果直接找不到，尝试在祖先中查找
-    final ancestor = context.getElementForInheritedWidgetOfExactType<MyInheritedWidget<T>>()?.widget;
-    if (ancestor is MyInheritedWidget<T>) {
-      return ancestor.model;
-    }
-
-    // 最后尝试：遍历查找任何 MyInheritedWidget
-    context.visitAncestorElements((element) {
-      final widget = element.widget;
-      if (widget is MyInheritedWidget && widget.model is T) {
-        return false; // 停止遍历
-      }
-      return true; // 继续遍历
-    });
-
-    print('❌ MyProvider.of<$T>: No MyInheritedWidget found in ancestors');
-    return null;
+  static T? of<T> (BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<MyInheritedWidget<T>>()?.model;
   }
 }
 
